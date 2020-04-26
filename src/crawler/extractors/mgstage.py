7b13f790e53669d00acac42aee5316818a7ccb97
caoyufei <atcaoyufei@gmail.com>
2020-04-26 19:58:17 +0800

    huawei

--- src/crawler/extractors/mgstage.py
+++ src/crawler/extractors/mgstage.py
@@ -49,13 +49,14 @@ class MgStage(BaseCrawler):
             'star': star,
             'tag': tag
         }
-        del data
+        # del data
 
         if not images:
             self.processing(kwargs.get('bar'), params['alias'], 'fail')
             return
 
-        self.processing(kwargs.get('bar'), params['alias'], 'done')
+        # self.processing(kwargs.get('bar'), params['alias'], 'done')
+        print('[{}/{}]: {}'.format(data['i'], data['n'], params.get('title')))
         self.data.append(params)
         if len(self.data) >= 50:
             DB.insert_all('ii_mgstage', self.data)
